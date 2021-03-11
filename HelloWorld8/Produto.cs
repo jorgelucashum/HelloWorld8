59c816b1c4569dce086f33025abce95a4c7c96d4
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-11 15:51:42 -0300

    Add project files.

---
+++ HelloWorld8/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization; // usado para converter 'CultureInfo.InvariantCulture' 
+namespace HelloWorld8
+{
+    class Produto
+    {
+        public string Nome; //atributo com primeira letra maiusculo dentro em classe fora da main
+        public int Quantidade;
+        public double Preco;
+
+        public double ValorTotalEmEsqtoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade; // Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade; // Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString() // método usado para retornar a váriavel 'p1' nas informações em String
+        {
+            return Nome + ", R$:" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " " +
+                "Unidades, total R$:" + ValorTotalEmEsqtoque().ToString("F2", CultureInfo.InvariantCulture); // converter as ',' para '.' (padrão internacional)
+        }
+
+    }
+}
---
+++ HelloWorld8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld8
+{
+    class Program
+    {
+        // toda classe em C# é uma subclasse de Object, já possuindo alguns métodos como:
+        // Equals: Compara se o objeto é igual ao outro
+        // ToString: converte o objeto para string 
+        // GetHashCode: retorna o hash do objeto
+        // GetType: retorna o tipo do objeto
+        static void Main(string[] args)
+        {
+            Produto p1;
+            p1 = new Produto();
+
+            Console.WriteLine("Digite o nome do produto: ");
+            p1.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o preço: ");
+            p1.Preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade: ");
+            p1.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do pruduto: " + p1);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a quantidade de produtos a serem adicionadas");
+            int quant = int.Parse(Console.ReadLine());
+            p1.AdicionarProdutos(quant);
+            Console.WriteLine("Dados atualizados: " + p1);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a quantidade de produtos a serem removidas");
+            quant = int.Parse(Console.ReadLine());
+            p1.RemoverProdutos(quant);
+            Console.WriteLine("Dados atualizados: " + p1);
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
